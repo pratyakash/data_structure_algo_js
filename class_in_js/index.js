a412920a499a967cdf969aca7078e0d43cb92ddc
pratyakash <pratyakash13@gmail.com>
2021-01-28 17:59:08 +0530

    Classes in JS

---
+++ class_in_js/index.js
@@ -0,0 +1,10 @@
+class Student {
+    constructor(first_name, last_name, year) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.year = year;
+    }
+}
+
+
+let first_student = new Student('Pratyakash', 'Saini', 2013);
